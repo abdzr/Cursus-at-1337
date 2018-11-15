ecc73bbabf6d05ea94f4d668f40c62bb311610b8
Abdelilah Zarzor <azarzor@e1r2p4.1337.ma>
2018-11-15 12:20:52 +0100

    Get Next Line

---
+++ Get_Next_Line/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azarzor <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/31 13:24:48 by azarzor           #+#    #+#             */
+/*   Updated: 2018/11/06 19:59:11 by azarzor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+
+# define BUFF_SIZE 1
+
+int		get_next_line(const int fd, char **line);
+
+#endif
